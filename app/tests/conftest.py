46f86d80a43b48a472f1882fd0658b049459c01c
mirenja <mirenjak@gmail.com>
2023-04-13 10:18:44 +0200

    initial test setup

---
+++ app/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from app.app import create_app
+
+@pytest.fixture
+def client():
+    app =  create_app()
+
+    with app.app_context():
+        yield app.test_client()
+#within the context of the applicatio do the following.
+#yielf functions as return but we can still run some code after its done running
