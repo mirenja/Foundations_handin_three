33afe792670cc2482a8b921c3c1ae8d52cfdc2a1
mirenja <mirenjak@gmail.com>
2023-04-19 11:14:38 +0200

    added api

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,16 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .services.serialize_posts import serialize_posts
 from ..articles.models import Articles, Authors
+from os import environ
 
 
 blueprint =  Blueprint('api',__name__)
 
 @blueprint.get('/api/v1/posts')
 def posts():
-    posts = Articles.query.all()
-    return jsonify(
+    if environ.get('API_KEY') == request.headers.get('X-API-KEY'):
+        posts = Articles.query.all()
+        return jsonify(
         serialize_posts(posts)
     )
+    else:
+        return jsonify({'error':'invalid API key'}),401
+
+    
+    
 
 
 #flask converts the json dictionary into JSOn string automatically
